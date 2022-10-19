dc9895cc6674dccb80d9db85d5b88cabdba897fc
canadasubash <canadasubash@gmail.com>
2022-10-19 17:09:33 +0530

    username code

--- src/test/java/org/test/TestJava.java
+++ src/test/java/org/test/TestJava.java
@@ -2,8 +2,13 @@ package org.test;
 
 public class TestJava {
 	
+	public static void userName() {
+		System.out.println("UserName");
+
+	}
+	
 	public static void main(String[] args) {
-		
+		userName();
 		System.out.println("Success Criteria");
 	}
 
