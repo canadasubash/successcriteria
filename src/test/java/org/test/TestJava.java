e2cf83c179aba55ba48d36e9117ca39954a9530d
canadasubash <canadasubash@gmail.com>
2022-10-19 17:19:49 +0530

    password code

--- src/test/java/org/test/TestJava.java
+++ src/test/java/org/test/TestJava.java
@@ -2,8 +2,13 @@ package org.test;
 
 public class TestJava {
 	
-	public static void main(String[] args) {
+	public static void password() {
+		System.out.println("test");
+
+	}
 		
+	public static void main(String[] args) {
+		password();
 		System.out.println("Success Criteria");
 	}
 
